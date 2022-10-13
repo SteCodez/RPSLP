0906049a07c727d6a34c12765a2917edd5fb27f5
Stephen McAuliffe <107943478+SteCodez@users.noreply.github.com>
2022-10-13 14:27:49 +0100

    Adding rules for what wins over what

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+const rule = {
+    Lizard: ["Spock", "Paper"],
+
+    Paper: ["Rock", "Spock"],
+
+    Rock: ["Lizard", "Scissor"],
+
+    Scissor: ["Paper", "Lizard"],
+
+    Spock: ["Scissor", "Rock"],
+
+}
